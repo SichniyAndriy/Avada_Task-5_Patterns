bfaf6be2b6e4c9352a9f229db6769193189262cb
Andriy Sichniy <sichniy.andriy@gmail.com>
2024-03-07 12:19:50 +0200

    add whirlpool brand

---
+++ src/creation/abstract_factory/model/whirlpool/WhirlpoolMicrowaveOven.java
@@ -0,0 +1,20 @@
+package creation.abstract_factory.model.whirlpool;
+
+import creation.abstract_factory.model.Appliance;
+
+public class WhirlpoolMicrowaveOven implements Appliance {
+    @Override
+    public void turnOn() {
+
+    }
+
+    @Override
+    public void use() {
+
+    }
+
+    @Override
+    public void turnOff() {
+
+    }
+}
