429a9d3f115c2f788fac05b07a5280a7f2901be0
Andriy Sichniy <sichniy.andriy@gmail.com>
2024-03-07 12:19:21 +0200

    add siemens brand

---
+++ src/creation/abstract_factory/model/siemens/SiemensVacuumCleaner.java
@@ -0,0 +1,20 @@
+package creation.abstract_factory.model.siemens;
+
+import creation.abstract_factory.model.Appliance;
+
+public class SiemensVacuumCleaner implements Appliance {
+    @Override
+    public void turnOn() {
+
+    }
+
+    @Override
+    public void use() {
+
+    }
+
+    @Override
+    public void turnOff() {
+
+    }
+}
