944c8e3a92b62d7c8de2f82fcf43446219f7d051
Andriy Sichniy <sichniy.andriy@gmail.com>
2024-03-07 13:03:48 +0200

    fix Appliance interface

--- src/creation/abstract_factory/model/Appliance.java
+++ src/creation/abstract_factory/model/Appliance.java
@@ -2,14 +2,14 @@ package creation.abstract_factory.model;
 
 public interface Appliance {
     default String turnOn() {
-        return "Вмикаю " + this.getClass();
+        return "Вмикаю " + this.getClass().getSimpleName();
     }
 
     default String use() {
-        return "Користуюсь " + this.getClass();
+        return "Користуюсь " + this.getClass().getSimpleName();
     }
 
     default String turnOff() {
-        return "Вимикаю" + this.getClass();
+        return "Вимикаю " + this.getClass().getSimpleName();
     }
 }
