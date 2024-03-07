d2f189de75108ed2b80c6eef663b8428f5f7b941
Andriy Sichniy <sichniy.andriy@gmail.com>
2024-03-07 12:19:11 +0200

    add samsung brand

---
+++ src/creation/abstract_factory/model/samsung/SamsungGasStove.java
@@ -0,0 +1,20 @@
+package creation.abstract_factory.model.samsung;
+
+import creation.abstract_factory.model.Appliance;
+
+public class SamsungGasStove implements Appliance {
+    @Override
+    public void turnOn() {
+
+    }
+
+    @Override
+    public void use() {
+
+    }
+
+    @Override
+    public void turnOff() {
+
+    }
+}
---
+++ src/creation/abstract_factory/model/samsung/SamsungMicrowaveOven.java
@@ -0,0 +1,20 @@
+package creation.abstract_factory.model.samsung;
+
+import creation.abstract_factory.model.Appliance;
+
+public class SamsungMicrowaveOven implements Appliance {
+    @Override
+    public void turnOn() {
+
+    }
+
+    @Override
+    public void use() {
+
+    }
+
+    @Override
+    public void turnOff() {
+
+    }
+}
