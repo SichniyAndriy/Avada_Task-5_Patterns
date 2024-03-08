fb080b875ac6fc78acf6b449fc3e4a523db87025
Andriy Sichniy <sichniy.andriy@gmail.com>
2024-03-08 11:20:24 +0200

    add client code in main

--- src/creation/builder/Main.java
+++ src/creation/builder/Main.java
@@ -1,7 +1,25 @@
 package creation.builder;
 
+import creation.builder.model.Tank;
+import creation.builder.service.HeavyTankBuilder;
+import creation.builder.service.LightTankBuilder;
+import creation.builder.service.MiddleTankBuilder;
+import creation.builder.service.TankBuilderDirector;
+
 public class Main {
     public static void main(String[] args) {
+        TankBuilderDirector director = new TankBuilderDirector();
 
+        LightTankBuilder lightTankBuilder = new LightTankBuilder();
+        MiddleTankBuilder middleTankBuilder = new MiddleTankBuilder();
+        HeavyTankBuilder heavyTankBuilder = new HeavyTankBuilder();
+
+        Tank lightTank = director.buildTank(lightTankBuilder);
+        Tank middleTank = director.buildTank(middleTankBuilder);
+        Tank heavyTank = director.buildTank(heavyTankBuilder);
+
+        System.out.println(lightTank.toString());
+        System.out.println(middleTank.toString());
+        System.out.println(heavyTank.toString());
     }
 }
