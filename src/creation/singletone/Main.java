d6c4f9b0d708c763bd4712c61857527f392774a1
Andriy Sichniy <sichniy.andriy@gmail.com>
2024-03-08 16:53:37 +0200

    add code in Main class

--- src/creation/singletone/Main.java
+++ src/creation/singletone/Main.java
@@ -1,7 +1,20 @@
 package creation.singletone;
 
+import creation.singletone.model.User;
+import java.util.Locale;
+import net.datafaker.Faker;
+
 public class Main {
     public static void main(String[] args) {
+        Faker faker = new Faker(Locale.getDefault());
+        User user1 = new User(faker.name().fullName());
+        User user2 = new User(faker.name().fullName());
+        User user3 = new User(faker.name().fullName());
+        User user4 = new User(faker.name().fullName());
 
+        System.out.println(user1.use(SingleUniqueTool.getINSTANCE()).getResult());
+        System.out.println(user2.use(SingleUniqueTool.getINSTANCE()).getResult());
+        System.out.println(user3.use(SingleUniqueTool.getINSTANCE()).getResult());
+        System.out.println(user4.use(SingleUniqueTool.getINSTANCE()).getResult());
     }
 }
