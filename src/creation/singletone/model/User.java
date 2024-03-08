efbd6683ad8218f706d359164b7c154216dae427
Andriy Sichniy <sichniy.andriy@gmail.com>
2024-03-08 16:53:10 +0200

    add User class

---
+++ src/creation/singletone/model/User.java
@@ -0,0 +1,18 @@
+package creation.singletone.model;
+
+import creation.singletone.SingleUniqueTool;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class User {
+    private String name;
+
+    public Result use(SingleUniqueTool tool) {
+        System.out.println(name + " use " + tool.getClass().getSimpleName() + " - " + tool.getName());
+        Result result = new Result();
+        System.out.print("Return ");
+        return result;
+    }
+}
