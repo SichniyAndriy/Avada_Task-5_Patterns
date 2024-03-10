fb012cfe2f5e3a50c9a14cba0c47b19312a0cbfa
Andriy Sichniy <sichniy.andriy@gmail.com>
2024-03-10 16:26:01 +0200

    add ReportGenerator interface

---
+++ src/creation/factory_method/service/ReportGenerator.java
@@ -0,0 +1,10 @@
+package creation.factory_method.service;
+
+import creation.factory_method.model.Report;
+
+public interface ReportGenerator {
+
+    boolean check(String text);
+
+    Report generate(String text);
+}
