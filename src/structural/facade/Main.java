4c37ecc75ed386c4d70f1dd3bb8f1c42c705220a
Andriy Sichniy <sichniy.andriy@gmail.com>
2024-03-14 13:09:04 +0200

    add client's code in main

--- src/structural/facade/Main.java
+++ src/structural/facade/Main.java
@@ -1,7 +1,33 @@
 package structural.facade;
 
-public class Main {
-    public static void main(String[] args) {
+import java.math.BigDecimal;
+import java.util.Locale;
+import net.datafaker.Faker;
+import structural.facade.model.RealOnlineStore;
 
+public class Main {
+    private final static Faker faker = new Faker(Locale.getDefault());
+
+    public static void main(String[] args) {
+        RealOnlineStore onlineStore = new RealOnlineStore();
+
+        onlineStore.addToBucket(
+                faker.commerce().productName(),
+                BigDecimal.valueOf(faker.random().nextDouble(100., 10000.))
+        );
+        onlineStore.addToBucket(
+                faker.commerce().productName(),
+                BigDecimal.valueOf(faker.random().nextDouble(100., 10000.))
+        );
+        onlineStore.addToBucket(
+                faker.commerce().productName(),
+                BigDecimal.valueOf(faker.random().nextDouble(100., 10000.))
+        );
+        onlineStore.addToBucket(
+                faker.commerce().productName(),
+                BigDecimal.valueOf(faker.random().nextDouble(100., 10000.))
+        );
+
+        onlineStore.buy();
     }
 }
