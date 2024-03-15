4a179aa7af4211baa530da7327da707ad37609bd
Andriy Sichniy <sichniy.andriy@gmail.com>
2024-03-15 16:44:29 +0200

    fix in model

--- src/creation/singletone/model/Result.java
+++ src/creation/singletone/model/Result.java
@@ -1,11 +1,9 @@
 package creation.singletone.model;
 
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import net.datafaker.Faker;
 
 @Getter
-@NoArgsConstructor
 public class Result {
     private final String result = new Faker().construction().materials();
 }
