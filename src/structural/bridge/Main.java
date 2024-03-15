482e142cdd46824fd7eac898718912fea345019a
Andriy Sichniy <sichniy.andriy@gmail.com>
2024-03-15 20:45:41 +0200

    fix bridge main

--- src/structural/bridge/Main.java
+++ src/structural/bridge/Main.java
@@ -1,6 +1,5 @@
 package structural.bridge;
 
-import java.util.List;
 import net.datafaker.Faker;
 import structural.bridge.model.RealComputer;
 import structural.bridge.model.drive.NVMeDrive;
@@ -16,30 +15,25 @@ import structural.bridge.model.videocard.NVidiaGraphicCard;
 public class Main {
     private final static Faker faker = new Faker();
     public static void main(String[] args) {
-        RealComputer computer1 = new RealComputer();
-        computer1.putProcessor(
+        RealComputer computer = new RealComputer();
+        computer.putProcessor(
                 new AmdProcessor(faker.computer().platform(),3_000_000_000L,2_000_000_000L, 16)
         );
-        computer1.putMemory(
+        computer.putMemory(
                 new DDR4Memory(2_400_000_000L, 16_000_000_000L)
         );
-        computer1.putDrive(
+        computer.putDrive(
                 new NVMeDrive(faker.computer().brand(),2_000_000_000_000L)
         );
-        computer1.putVideoCard(
+        computer.putVideoCard(
                 new NVidiaGraphicCard(16_000_000_000L, 8_000_000_000L)
         );
-        computer1.install(new TextEditor(faker.funnyName().name(), faker.code().isbn13()));
-        computer1.install(new GraphicEditor(faker.funnyName().name(), faker.code().isbn10()));
-        computer1.install(new TextEditor(faker.funnyName().name(), faker.code().ean13()));
-        computer1.install(new Browser(faker.funnyName().name(), faker.code().isbnGs1()));
-        computer1.install(new Game(faker.funnyName().name(), faker.gameOfThrones().dragon()));
+        computer.install(new TextEditor(faker.funnyName().name(), faker.code().isbn13()));
+        computer.install(new GraphicEditor(faker.funnyName().name(), faker.code().isbn10()));
+        computer.install(new TextEditor(faker.funnyName().name(), faker.code().ean13()));
+        computer.install(new Browser(faker.funnyName().name(), faker.code().isbnGs1()));
+        computer.install(new Game(faker.funnyName().name(), faker.gameOfThrones().dragon()));
 
-        List<AbstractProgram> programList = computer1.getPrograms().stream()
-                .map(p -> (AbstractProgram) p)
-                .toList();
-        for (int i = 0; i < 3; ++i) {
-            computer1.start(programList.get(faker.random().nextInt(0, programList.size() - 1)).getName());
-        }
+        computer.programs.forEach(prog -> computer.start(((AbstractProgram) prog).getName()));
     }
 }
