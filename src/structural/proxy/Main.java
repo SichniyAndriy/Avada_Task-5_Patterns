db0d79049c13bb7bea7c98fbbbfae6ebdb14c387
Andriy Sichniy <sichniy.andriy@gmail.com>
2024-03-15 11:25:34 +0200

    add client's code

--- src/structural/proxy/Main.java
+++ src/structural/proxy/Main.java
@@ -1,7 +1,19 @@
 package structural.proxy;
 
+import structural.proxy.model.File;
+import structural.proxy.model.RealFileProxy;
+
 public class Main {
     public static void main(String[] args) {
+        File file1 = new RealFileProxy("filename1");
+        File file2 = new RealFileProxy("filename2");
+        File file3 = new RealFileProxy("filename3");
 
+        file1.display();
+        file2.display();
+        file3.display();
+        file1.display();
+        file2.display();
+        file3.display();
     }
 }
