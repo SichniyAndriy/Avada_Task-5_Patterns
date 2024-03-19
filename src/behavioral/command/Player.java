cf954179c84924f14578d21411437bf73aad4a78
Andriy Sichniy <sichniy.andriy@gmail.com>
2024-03-19 14:48:39 +0200

    add Player class

---
+++ src/behavioral/command/Player.java
@@ -0,0 +1,15 @@
+package behavioral.command;
+
+public class Player {
+    public void play() {
+        System.out.println("Play");
+    }
+
+    public void pause() {
+        System.out.println("Pause");
+    }
+
+    public void stop() {
+        System.out.println("Stop");
+    }
+}
