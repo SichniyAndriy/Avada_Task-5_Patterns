b1511d4c2efbb30f1260aa6c07d2dfce65fe9ecf
Andriy Sichniy <sichniy.andriy@gmail.com>
2024-03-19 19:55:35 +0200

    add client's code in main

--- src/behavioral/interpreter/Main.java
+++ src/behavioral/interpreter/Main.java
@@ -1,7 +1,38 @@
 package behavioral.interpreter;
 
+import behavioral.interpreter.model.Context;
+import behavioral.interpreter.model.expression.AndExpression;
+import behavioral.interpreter.model.expression.CreateExpression;
+import behavioral.interpreter.model.expression.DeleteExpression;
+import behavioral.interpreter.model.expression.ReadExpression;
+
 public class Main {
     public static void main(String[] args) {
+        Context context = new Context();
+
+        System.out.println(new CreateExpression(context).interpret());
+        System.out.println(new CreateExpression(context, 100).interpret());
+        System.out.println(new CreateExpression(context, context.generateNewPerson()).interpret());
+
+        System.out.println(new AndExpression(
+                        new CreateExpression(context, 50),
+                        new ReadExpression(context, 100, 150)
+        ).interpret());
+
+        System.out.println(new ReadExpression(context, 25).interpret());
+        System.out.println(new ReadExpression(context, 50).interpret());
+        System.out.println(new ReadExpression(context, 100).interpret());
+        System.out.println(new ReadExpression(context, 150).interpret());
+        System.out.println(new ReadExpression(context, 15, 75).interpret());
+
+        System.out.println(new AndExpression(
+                        new ReadExpression(context, 45),
+                        new DeleteExpression(context, 45)
+        ).interpret());
+
+        System.out.println(new DeleteExpression(context, 25).interpret());
+        System.out.println(new DeleteExpression(context, 10, 90).interpret());
+
 
     }
 }
