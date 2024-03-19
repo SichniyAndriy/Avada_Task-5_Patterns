3697c27158739068da5fd19cfaa6902e8aba7905
Andriy Sichniy <sichniy.andriy@gmail.com>
2024-03-19 14:49:32 +0200

    add client's code in main

--- src/behavioral/command/Main.java
+++ src/behavioral/command/Main.java
@@ -1,7 +1,24 @@
 package behavioral.command;
 
+import behavioral.command.model.control.RemoteControl;
+import behavioral.command.model.control.TouchControl;
+
 public class Main {
     public static void main(String[] args) {
+        Player player = new Player();
 
+        System.out.println("Remote control");
+        RemoteControl remoteControl = new RemoteControl(player);
+        remoteControl.play();
+        remoteControl.pause();
+        remoteControl.play();
+        remoteControl.stop();
+
+        System.out.println("Touch control");
+        TouchControl touchControl = new TouchControl(player);
+        touchControl.play();
+        touchControl.pause();
+        touchControl.play();
+        touchControl.stop();
     }
 }
