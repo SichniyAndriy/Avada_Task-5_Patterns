a63230cc343dbec2fbf4ea0e8978d92e3de1eeaf
Andriy Sichniy <sichniy.andriy@gmail.com>
2024-03-20 13:45:44 +0200

    add client's code in main

---
+++ src/behavioral/mediator/Main.java
@@ -0,0 +1,22 @@
+package behavioral.mediator;
+
+import behavioral.mediator.model.FlightControlCenter;
+import behavioral.mediator.model.FlightDispatcher;
+import behavioral.mediator.model.Plane;
+import java.util.Locale;
+import net.datafaker.Faker;
+
+public class Main {
+    public final static Faker faker = new Faker(Locale.getDefault());
+
+    public static void main(String[] args) {
+        FlightControlCenter flightDispatcher = new FlightDispatcher();
+        int len = faker.random().nextInt(10, 20);
+        for (int i = 0; i < len; ++i) {
+            Plane plane = new Plane(faker.aviation().airplane());
+            plane.register(flightDispatcher);
+        }
+
+        flightDispatcher.sendAll("Hi!");
+    }
+}
