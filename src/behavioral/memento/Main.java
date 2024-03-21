2aa96685fad7e4ad55857e1b972cf0598d65e31b
Andriy Sichniy <sichniy.andriy@gmail.com>
2024-03-21 11:50:21 +0200

    add client's code in main

--- src/behavioral/memento/Main.java
+++ src/behavioral/memento/Main.java
@@ -1,7 +1,28 @@
 package behavioral.memento;
 
-public class Main {
-    public static void main(String[] args) {
+import behavioral.memento.model.Game;
+import behavioral.memento.model.GameCaretaker;
+import java.util.Locale;
+import net.datafaker.Faker;
 
+public class Main {
+    public static Faker faker = new Faker(Locale.getDefault());
+
+    public static void main(String[] args) {
+        Game game = new Game();
+        GameCaretaker gameCaretaker = new GameCaretaker();
+
+        int len = faker.random().nextInt(5, 15);
+        for (int i = 0; i < len; i++) {
+            game.increaseLevel();
+            game.increaseScore(faker.random().nextInt(25, 75));
+            gameCaretaker.addSave(game.save());
+        }
+
+        gameCaretaker.showSaves();
+
+        game.showState();
+        game.restore(gameCaretaker.getSaveByIndex(faker.random().nextInt(len)));
+        game.showState();
     }
 }
