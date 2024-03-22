2ee93bab9fd407f7ec38525ac624b1dcc0cb266b
Andriy Sichniy <sichniy.andriy@gmail.com>
2024-03-22 11:09:32 +0200

    add client's code in main

--- src/behavioral/template_method/Main.java
+++ src/behavioral/template_method/Main.java
@@ -1,7 +1,15 @@
 package behavioral.template_method;
 
+import behavioral.template_method.model.CarWorkshop;
+import behavioral.template_method.model.ElectronicWorkshop;
+import behavioral.template_method.model.MeetWorkshop;
+import behavioral.template_method.model.ToyWorkshop;
+
 public class Main {
     public static void main(String[] args) {
-
+        new CarWorkshop().create();
+        new ElectronicWorkshop().create();
+        new MeetWorkshop().create();
+        new ToyWorkshop().create();
     }
 }
