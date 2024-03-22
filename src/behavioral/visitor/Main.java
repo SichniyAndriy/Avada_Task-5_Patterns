33c5f2cfd6eb4e8c23d3d42a2b212850fd210e64
Andriy Sichniy <sichniy.andriy@gmail.com>
2024-03-22 12:34:07 +0200

    add client's code in main

---
+++ src/behavioral/visitor/Main.java
@@ -0,0 +1,24 @@
+package behavioral.visitor;
+
+import behavioral.visitor.model.CarDiagnostic;
+import behavioral.visitor.model.CarVisitor;
+import behavioral.visitor.model.car.AbstractCarElement;
+import behavioral.visitor.model.car.Body;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<AbstractCarElement> components = new ArrayList<>();
+        components.add(new Body("Кузов"));
+        components.add(new Body("Двигун"));
+        components.add(new Body("Колесо 1"));
+        components.add(new Body("Колесо 2"));
+        components.add(new Body("Колесо 3"));
+        components.add(new Body("Колесо 4"));
+
+        CarVisitor visitor = new CarDiagnostic();
+
+        components.forEach(c -> c.accept(visitor));
+    }
+}
